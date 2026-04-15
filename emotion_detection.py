4cf1b3138398639aa4ea945fb9c6011b3a963453
JEAN LOUIS DOS SANTOS <71648706+jeanlouisdossantos@users.noreply.github.com>
2026-04-15 19:33:46 +0000

    Mise à jour de la fonction emotion_detector pour retourner un objet JSON au lieu d'une chaîne de caractères

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -5,4 +6,4 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
-    return response.text
+    return json.loads(response.text)
